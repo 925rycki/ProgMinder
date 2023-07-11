3af7597e9acd513037a2602f7e278f18591f1297
925rycki <925rycki@gmail.com>
2023-07-11 15:11:19 +0900

    usersにカラムを追加

--- backend/db/migrate/20230408074932_add_columns_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :profile, :string, limit: 255, after: :name
-  end
-
-  remove_column :users, :nickname, :string
-end
