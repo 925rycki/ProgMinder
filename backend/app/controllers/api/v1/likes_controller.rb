725dc026c9eba739f03537c33891d25fac0370bf
925rycki <925rycki@gmail.com>
2023-07-30 11:14:52 +0900

    jb導入

--- backend/app/controllers/api/v1/likes_controller.rb
+++ backend/app/controllers/api/v1/likes_controller.rb
@@ -1,28 +1,30 @@
 class Api::V1::LikesController < ApplicationController
   def create
     if current_api_v1_user
-      like = current_api_v1_user.likes.build(report_id: params[:report_id])
-      if like.save
-        render json: like, status: :created
+      @like = current_api_v1_user.likes.build(report_id: params[:report_id])
+      if @like.save
+        render 'create', status: :created
       else
-        render json: like.errors, status: :unprocessable_entity
+        render 'create', status: :unprocessable_entity
       end
     else
-      render json: { message: "ログインしてください" }, status: :unauthorized
+      @message = "ログインしてください"
+      render 'destroy', status: :unauthorized
     end
   end
 
   def destroy
     if current_api_v1_user
-      like = Like.find_by(user_id: current_api_v1_user.id, report_id: params[:id])
-      if like&.destroy
-        render json: { message: "Like削除成功" }, status: :ok
+      @like = Like.find_by(user_id: current_api_v1_user.id, report_id: params[:id])
+      if @like&.destroy
+        head :no_content
       else
-        render json: { message: "Like削除失敗" }, status: :not_found
+        @message = "Like削除失敗"
+        render 'destroy', status: :not_found
       end
     else
-      render json: { message: "ログインしてください" }, status: :unauthorized
+      @message = "ログインしてください"
+      render 'destroy', status: :unauthorized
     end
   end
-
 end
