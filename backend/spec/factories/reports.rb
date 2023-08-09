535a1cd38f4ffd604b720b1395def29829d8bbe6
925rycki <925rycki@gmail.com>
2023-08-09 22:29:39 +0900

    テストコードを追加

--- backend/spec/factories/reports.rb
+++ backend/spec/factories/reports.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
   factory :report do
+    created_date { "2023-01-01" }
+    todays_goal { "Sample todays goal" }
+    study_time { 1 }
+    goal_review { "Sample goal review" }
+    challenges { "Sample challenges" }
+    learnings { "Sample learnings" }
+    thoughts { "Sample thoughts" }
+    tomorrows_goal { "Sample tomorrow goal" }
+    user
   end
 end
+
--- backend/spec/requests/api/v1/reports_spec.rb
+++ backend/spec/requests/api/v1/reports_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Reports" do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Api::V1::Reports", type: :request do
+  describe "GET /api/v1/reports" do
+    before do
+      @user = create(:user)
+      3.times { create(:report, user: @user) }
+      get api_v1_reports_path, as: :json
+    end
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns all reports' do
+      reports = JSON.parse(response.body)
+      expect(reports.size).to eq(3)
+    end
   end
 end
