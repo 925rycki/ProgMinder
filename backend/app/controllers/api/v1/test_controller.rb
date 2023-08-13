dfe547dda85c16bffe31332f4dfae5bce3993492
925rycki <925rycki@gmail.com>
2023-08-13 15:34:59 +0900

    自動デプロイの動作確認

--- backend/app/controllers/api/v1/test_controller.rb
+++ backend/app/controllers/api/v1/test_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TestController < ApplicationController
       def index
-        render json: { message: 'Hello World!4' }
+        render json: { message: 'Hello World!2' }
       end
     end
   end
