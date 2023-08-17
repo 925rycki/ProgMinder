66ac6ccb5514233dc84feb8814b49c6bb31ba554
925rycki <925rycki@gmail.com>
2023-08-17 11:03:57 +0900

    URLを更新

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
-  # config.asset_host = "https://api.equipment-build.com"
-  config.asset_host = "http://localhost:3001"
+  config.asset_host = "https://api.progminder.tech"
+  # config.asset_host = "http://localhost:3001"
   config.storage = :file
   config.cache_storage = :file
 end
